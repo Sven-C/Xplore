113f6a4373c68060a38a330fa855a13e3d4afba7
Sven Cuyt <svencuyt@telenet.be>
2023-06-09 08:57:32 +0200

    Make c source hooks log arguments and result

--- src/hooks/c/fopen_hook.c
+++ src/hooks/c/fopen_hook.c
@@ -1,6 +1,10 @@
+#include <stdint.h>
 #include <stdlib.h>
 #include <stdio.h>
 
 FILE* hook_fopen(char* filename, char* mode) {
-    return NULL;
+    FILE* result = fopen(filename, mode);
+    int *fd = (int*) (((uint8_t*) result) + 0x70);
+    printf("fopen(%s, %s) = %p (open file %p with fd %d)\n", filename, mode, result, result, *fd);
+    return result;
 }
--- src/hooks/c/fread_hook.c
+++ src/hooks/c/fread_hook.c
@@ -1,6 +1,10 @@
+#include <stdint.h>
 #include <stdlib.h>
 #include <stdio.h>
 
 size_t hook_fread(void* buffer, size_t element_size, size_t count, FILE* file) {
-    return 0;
+    size_t bytes_read = fread(buffer, element_size, count, file);
+    int *fd = (int*) (((uint8_t*) file) + 0x70);
+    printf("fread(%p, %lu, %lu, %p) = %lu (read %lu bytes from file with fd %d\n", buffer, element_size, count, file, bytes_read, bytes_read, *fd);
+    return bytes_read;
 }
