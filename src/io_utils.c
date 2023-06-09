8637f69f73175923b253e84719ef9d05b61915cf
Sven Cuyt <svencuyt@telenet.be>
2023-06-09 10:29:49 +0200

    Fix buggy read_file implementation
    When reading pseudo-files such as /proc/<pid>/maps, stat() returns 0 for the filesize.
    This causes the application to get stuck in an infinite loop.

--- src/io_utils.c
+++ src/io_utils.c
@@ -11,9 +11,14 @@ struct buffer read_file(char* filename) {
     struct buffer buffer;
     struct stat st;
     stat(filename, &st);
-    uint8_t* content = malloc(st.st_size);
+    size_t buffer_size = st.st_size;
+    // This check is needed to properly read pseudo-files located in the /proc/ pseudo-filesystem.
+    if (buffer_size == 0) {
+        buffer_size = 1024;
+    }
+    uint8_t* content = malloc(buffer_size);
     buffer.content = content;
-    buffer.size = st.st_size;
+    buffer.size = buffer_size;
     printf("Reading file %s (%ld bytes)\n", filename, buffer.size);
     size_t copied = 0;
     FILE* file = fopen(filename, "r");
@@ -21,8 +26,9 @@ struct buffer read_file(char* filename) {
         printf("Failed to open %s (error %d: %s)\n", filename, errno, strerror(errno));
         exit(1);
     }
-    while (!feof(file) && !ferror(file)) {
-        copied += fread(buffer.content + copied, sizeof(uint8_t), buffer.size, file);
+    // For pseudo-files, e.g. /proc/<pid>/maps, feof(file) will return false, while we have already fully read the buffer.
+    while (!feof(file) && !ferror(file) && copied < buffer.size) {
+        copied += fread(buffer.content + copied, sizeof(uint8_t), buffer.size - copied, file);
     }
     if (ferror(file)) {
         printf("Error while reading %s (error %d: %s)\n", filename, ferror(file), strerror(ferror(file)));
